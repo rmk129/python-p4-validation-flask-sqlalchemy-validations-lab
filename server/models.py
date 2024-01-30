9004a4e746a82e8ef932001416ae1f1bb22a6a86
Romil <romil.m.kothari@gmail.com>
2024-01-29 20:04:47 -0500

    tests completed

--- server/models.py
+++ server/models.py
@@ -12,6 +12,20 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name field is required.")
+        author = db.session.query(Author.id).filter_by(name = name).first()
+        if author is not None:
+            raise ValueError("Name must be unique.")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +42,41 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+        y = ""
+        click_bait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not title:
+            raise ValueError("Needs a title")
+        for c in click_bait:
+            if c in title:
+                y = "yes"
+        if not y:
+            raise ValueError("Click Bait Error")
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Needs to be 250 characters or more")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Too Long")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        categories = ["Fiction", "Non-Fiction"]
+        y = ""
+        for c in categories:
+            if category == c:
+                y = "yes"
+        if not y:
+            raise ValueError("Need to be in the category")
+        return category
 
 
     def __repr__(self):
